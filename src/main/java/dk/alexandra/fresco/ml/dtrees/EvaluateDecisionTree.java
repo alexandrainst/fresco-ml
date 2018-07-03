a4c8be3c1cc37675a09bdd427006d3d664fedc4a
Tore Kasper Frederiksen <tore.frederiksen@alexandra.dk>
2018-07-03 16:57:11 +0200

    Cleaned up code for decision tree

--- src/main/java/dk/alexandra/fresco/ml/dtrees/EvaluateDecisionTree.java
+++ src/main/java/dk/alexandra/fresco/ml/dtrees/EvaluateDecisionTree.java
@@ -40,40 +40,37 @@ public class EvaluateDecisionTree implements Computation<SInt, ProtocolBuilderNu
       List<DRes<SInt>> lessThanFlags = new ArrayList<>(weights.size());
       List<DRes<SInt>> partialVal = new ArrayList<>(weights.size());
       for (int i = 0; i < weights.size(); i++) {
+        // Compute lessThan for each node in the tree, based on the selected feature
         lessThanFlags.add(par.comparison().compareLT(selectedFeatures.get(i), weights.get(i)));
+        // Construct a placeholder list used in the process of computing the final output
         partialVal.add(par.numeric().known(BigInteger.ONE));
       }
       return () -> new Pair<>(new Pair<>(lessThanFlags, partialVal), 1);
     }).whileLoop(pair -> pair.getSecond() <= treeModel.getDepth() - 2, (prevPar, pair) -> prevPar
         .par(par -> {
           List<DRes<SInt>> lessThanFlags = pair.getFirst().getFirst();
-          // Preprocess partial indicator bits
           List<DRes<SInt>> partialVal = pair.getFirst().getSecond();
-          // TODO is it necessary to construct a new object
-          List<DRes<SInt>> newPartialVal = new ArrayList<>(partialVal.size());
-          for (int i = 0; i < partialVal.size(); i++) {
-            newPartialVal.add(partialVal.get(i)); // placeholder
-          }
-          // Iterate over the chunks
-          // The index i will be the chunk size
-          // for (int i = 1; i < treeModel.getDepth(); i = 2 * i) {
+          // Let i be an exponentially increasing chunks size
           int i = pair.getSecond();
-          // Iterate over relevant layers
+          // Iterate over the chunks in the tree
           for (int j = i - 1; j <= treeModel.getDepth() - 2; j = j + 2 * i) {
-            // Iterate over the elements in the layer
+            // Compute the amount of element in the current layer
             int layerSize = Math.min((1 << (j + i)), (1 << (treeModel.getDepth() - 2)));
+            // Iterate over the elements in the layer of the given chunk
             for (int k = 0; k < layerSize; k++) {
               // Find the index of the deepest node already computed in the subtree
               int currentLeafIdx = layerSize + k;
+              // Compute the index of parent of the chunk
               int parentNodeIdx = (currentLeafIdx / (1 << i));
+              // Compute the index of the highest element in the chunk-subtree
               int subtreeNodeIdx = (currentLeafIdx / (1 << (i - 1)));
-              // Adjust for 0-indexing
               // Check if we are left subtree
               DRes<SInt> upperNode = subtreeNodeIdx % 2 == 0 ? par.logical().not(lessThanFlags.get(
                   parentNodeIdx - 1)) : lessThanFlags.get(parentNodeIdx - 1);
               // Compute partial product for upper subtree
               DRes<SInt> upperPartial = par.logical().and(upperNode, partialVal.get(parentNodeIdx
                   - 1));
+              // Finally compute the partial value of the current node in the chunk
               DRes<SInt> newPartial = par.logical().and(upperPartial, partialVal.get(currentLeafIdx
                   - 1));
               partialVal.set(currentLeafIdx - 1, newPartial);
@@ -82,19 +79,19 @@ public class EvaluateDecisionTree implements Computation<SInt, ProtocolBuilderNu
           return () -> new Pair<>(new Pair<>(lessThanFlags, partialVal), 2 * pair.getSecond());
         })).par((par, res) -> {
           DRes<SInt> currentRes = null;
+          // Compute the correct category by iterating over each of them
           for (int i = 0; i < treeModel.getCategories().size(); i++) {
             List<DRes<SInt>> lessThanFlags = res.getFirst().getFirst();
-            // Preprocess partial indicator bits
             List<DRes<SInt>> partialVal = res.getFirst().getSecond();
-            // Compute indicator bit for category by first selecting parent index
+            // Find the index of the inner node that is the parent of the i'th leaf (category)
             int parentNodeIdx = (1 << (treeModel.getDepth() - 2)) + (i / 2);
-            // Negate parent bit if needed
+            // Negate the bit of the parent in case the leaf (category) is a left child
             DRes<SInt> parentIndicator = i % 2 == 0 ? par.logical().not(lessThanFlags.get(
                 parentNodeIdx - 1)) : lessThanFlags.get(parentNodeIdx - 1);
             // Then compute final indicator for the given leaf (category)
             DRes<SInt> finalIndicator = par.logical().and(parentIndicator, partialVal.get(
                 parentNodeIdx - 1));
-            // Multiply indicator with category
+            // Multiply indicator bit with category value
             DRes<SInt> temp = par.numeric().mult(finalIndicator, treeModel.getCategories().get(i));
             // Compute partial sum
             if (currentRes == null) {
