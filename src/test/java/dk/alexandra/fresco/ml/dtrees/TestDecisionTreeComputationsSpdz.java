4d0013ec0a8be0cfaf30389fa7bea5e7557aac0e
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-08-17 13:54:40 +0200

    Spdz tests

---
+++ src/test/java/dk/alexandra/fresco/ml/dtrees/TestDecisionTreeComputationsSpdz.java
@@ -0,0 +1,45 @@
+package dk.alexandra.fresco.ml.dtrees;
+
+import dk.alexandra.fresco.framework.sce.evaluator.EvaluationStrategy;
+import dk.alexandra.fresco.suite.spdz.AbstractSpdzTest;
+import dk.alexandra.fresco.suite.spdz.configuration.PreprocessingStrategy;
+import org.junit.Test;
+
+public class TestDecisionTreeComputationsSpdz extends AbstractSpdzTest {
+
+  @Test
+  public void testInputDecisionTree() {
+    runTest(new DecisionTreeComputationTests.TestInputDecisionTree<>(),
+        EvaluationStrategy.SEQUENTIAL,
+        PreprocessingStrategy.DUMMY, 2);
+  }
+
+  @Test
+  public void testEvaluateDecisionTree() {
+    runTest(new DecisionTreeComputationTests.TestEvaluateDecisionTree<>(),
+        EvaluationStrategy.SEQUENTIAL,
+        PreprocessingStrategy.DUMMY, 2);
+  }
+
+  @Test
+  public void testEvaluateDecisionTreeFour() {
+    runTest(new DecisionTreeComputationTests.TestEvaluateDecisionTreeFour<>(),
+        EvaluationStrategy.SEQUENTIAL,
+        PreprocessingStrategy.DUMMY, 2);
+  }
+
+  @Test
+  public void testEvaluateDecisionTreeFive() {
+    runTest(new DecisionTreeComputationTests.TestEvaluateDecisionTreeFive<>(),
+        EvaluationStrategy.SEQUENTIAL,
+        PreprocessingStrategy.DUMMY, 2);
+  }
+
+  @Test
+  public void testEvaluateDecisionTreeSix() {
+    runTest(new DecisionTreeComputationTests.TestEvaluateDecisionTreeSix<>(),
+        EvaluationStrategy.SEQUENTIAL,
+        PreprocessingStrategy.DUMMY, 2);
+  }
+  
+}
