adb919bbc019fe1114a409e7b01b8d08633cebf0
Nikolaj Volgushev <n1v0lg@users.noreply.github.com>
2018-08-20 11:00:00 +0200

    Fix scoping issue part 1

--- src/main/java/dk/alexandra/fresco/ml/dtrees/EvaluateDecisionTree.java
+++ src/main/java/dk/alexandra/fresco/ml/dtrees/EvaluateDecisionTree.java
@@ -43,9 +43,12 @@ public class EvaluateDecisionTree implements Computation<SInt, ProtocolBuilderNu
         // Compute lessThan for each node in the tree, based on the selected feature
         lessThanFlags.add(par.comparison().compareLT(selectedFeatures.get(i), weights.get(i)));
         // Construct a placeholder list used in the process of computing the final output
+        // TODO should this be close?
         partialVal.add(par.numeric().known(BigInteger.ONE));
       }
-      return () -> new Pair<>(new Pair<>(lessThanFlags, partialVal), 1);
+      Pair<Pair<List<DRes<SInt>>, List<DRes<SInt>>>, Integer> pairIntegerPair = new Pair<>(
+          new Pair<>(lessThanFlags, partialVal), 1);
+      return () -> pairIntegerPair;
     }).whileLoop(pair -> pair.getSecond() < treeModel.getDepth() - 1, (prevPar, pair) -> prevPar
         .par(par -> {
           List<DRes<SInt>> lessThanFlags = pair.getFirst().getFirst();
@@ -66,43 +69,49 @@ public class EvaluateDecisionTree implements Computation<SInt, ProtocolBuilderNu
               int subtreeNodeIdx = subtreeLayerSize + ((k * subtreeLayerSize) / layerSize);
               // Compute the index of parent of the chunk
               int parentNodeIdx = subtreeNodeIdx / 2;
-              // Check if we are left subtree
-              DRes<SInt> upperNode = subtreeNodeIdx % 2 == 0 ? par.logical().not(lessThanFlags.get(
-                  parentNodeIdx - 1)) : lessThanFlags.get(parentNodeIdx - 1);
-              // Compute partial product for upper subtree
-              DRes<SInt> upperPartial = par.logical().and(upperNode, partialVal.get(parentNodeIdx
-                  - 1));
-              // Finally compute the partial value of the current node in the chunk
-              DRes<SInt> newPartial = par.logical().and(upperPartial, partialVal.get(currentLeafIdx
-                  - 1));
-              partialVal.set(currentLeafIdx - 1, newPartial);
+              // TODO this is hacky, find cleaner way
+              par.seq(seq -> {
+                // this is a different scope and the state has changed...
+                DRes<SInt> upperNode =
+                    subtreeNodeIdx % 2 == 0 ? seq.logical().not(lessThanFlags.get(
+                        parentNodeIdx - 1)) : lessThanFlags.get(parentNodeIdx - 1);
+                // Compute partial product for upper subtree
+                DRes<SInt> upperPartial = seq.logical().and(upperNode,
+                    partialVal.get(parentNodeIdx - 1));
+                DRes<SInt> newPartial = seq.logical()
+                    .and(upperPartial, partialVal.get(currentLeafIdx - 1));
+                partialVal.set(currentLeafIdx - 1, newPartial);
+                return null;
+              });
             }
           }
-          return () -> new Pair<>(new Pair<>(lessThanFlags, partialVal), 2 * pair.getSecond());
-        })).par((par, res) -> {
-          DRes<SInt> currentRes = null;
-          // Compute the correct category by iterating over each of them
-          for (int i = 0; i < treeModel.getCategories().size(); i++) {
-            List<DRes<SInt>> lessThanFlags = res.getFirst().getFirst();
-            List<DRes<SInt>> partialVal = res.getFirst().getSecond();
-            // Find the index of the inner node that is the parent of the i'th leaf (category)
-            int parentNodeIdx = (1 << (treeModel.getDepth() - 2)) + (i / 2);
-            // Negate the bit of the parent in case the leaf (category) is a left child
-            DRes<SInt> parentIndicator = i % 2 == 0 ? par.logical().not(lessThanFlags.get(
-                parentNodeIdx - 1)) : lessThanFlags.get(parentNodeIdx - 1);
-            // Then compute final indicator for the given leaf (category)
-            DRes<SInt> finalIndicator = par.logical().and(parentIndicator, partialVal.get(
-                parentNodeIdx - 1));
-            // Multiply indicator bit with category value
-            DRes<SInt> temp = par.numeric().mult(finalIndicator, treeModel.getCategories().get(i));
-            // Compute partial sum
-            if (currentRes == null) {
-              currentRes = temp;
-            } else {
-              currentRes = par.numeric().add(currentRes, temp);
-            }
-          }
-          return currentRes;
-        });
+          Pair<Pair<List<DRes<SInt>>, List<DRes<SInt>>>, Integer> pairIntegerPair = new Pair<>(
+              new Pair<>(lessThanFlags, partialVal), 2 * pair.getSecond());
+          return () -> pairIntegerPair;
+        })).seq((par, res) -> {
+      DRes<SInt> currentRes = null;
+      // Compute the correct category by iterating over each of them
+      for (int i = 0; i < treeModel.getCategories().size(); i++) {
+        List<DRes<SInt>> lessThanFlags = res.getFirst().getFirst();
+        List<DRes<SInt>> partialVal = res.getFirst().getSecond();
+        // Find the index of the inner node that is the parent of the i'th leaf (category)
+        int parentNodeIdx = (1 << (treeModel.getDepth() - 2)) + (i / 2);
+        // Negate the bit of the parent in case the leaf (category) is a left child
+        DRes<SInt> parentIndicator = i % 2 == 0 ? par.logical().not(lessThanFlags.get(
+            parentNodeIdx - 1)) : lessThanFlags.get(parentNodeIdx - 1);
+        // Then compute final indicator for the given leaf (category)
+        DRes<SInt> finalIndicator = par.logical().and(parentIndicator, partialVal.get(
+            parentNodeIdx - 1));
+        // Multiply indicator bit with category value
+        DRes<SInt> temp = par.numeric().mult(finalIndicator, treeModel.getCategories().get(i));
+        // Compute partial sum
+        if (currentRes == null) {
+          currentRes = temp;
+        } else {
+          currentRes = par.numeric().add(currentRes, temp);
+        }
+      }
+      return currentRes;
+    });
   }
 }
--- src/test/java/dk/alexandra/fresco/ml/dtrees/TestDecisionTreeComputationsSpdz.java
+++ src/test/java/dk/alexandra/fresco/ml/dtrees/TestDecisionTreeComputationsSpdz.java
@@ -10,41 +10,34 @@ public class TestDecisionTreeComputationsSpdz extends AbstractSpdzTest {
   @Test
   public void testInputDecisionTree() {
     runTest(new DecisionTreeComputationTests.TestInputDecisionTree<>(),
-        EvaluationStrategy.SEQUENTIAL,
+        EvaluationStrategy.SEQUENTIAL_BATCHED,
         PreprocessingStrategy.DUMMY, 2);
   }
 
   @Test
   public void testEvaluateDecisionTree() {
     runTest(new DecisionTreeComputationTests.TestEvaluateDecisionTree<>(),
-        EvaluationStrategy.SEQUENTIAL,
+        EvaluationStrategy.SEQUENTIAL_BATCHED,
         PreprocessingStrategy.DUMMY, 2);
   }
 
   @Test
   public void testEvaluateDecisionTreeFour() {
     runTest(new DecisionTreeComputationTests.TestEvaluateDecisionTreeFour<>(),
-        EvaluationStrategy.SEQUENTIAL,
+        EvaluationStrategy.SEQUENTIAL_BATCHED,
         PreprocessingStrategy.DUMMY, 2);
   }
 
   @Test
   public void testEvaluateDecisionTreeFive() {
     runTest(new DecisionTreeComputationTests.TestEvaluateDecisionTreeFive<>(),
-        EvaluationStrategy.SEQUENTIAL,
+        EvaluationStrategy.SEQUENTIAL_BATCHED,
         PreprocessingStrategy.DUMMY, 2);
   }
 
   @Test
   public void testEvaluateDecisionTreeSix() {
     runTest(new DecisionTreeComputationTests.TestEvaluateDecisionTreeSix<>(),
-        EvaluationStrategy.SEQUENTIAL,
-        PreprocessingStrategy.DUMMY, 2);
-  }
-
-  @Test
-  public void testEvaluateDecisionTreeBroken() {
-    runTest(new DecisionTreeComputationTests.TestEvaluateDecisionTree<>(),
         EvaluationStrategy.SEQUENTIAL_BATCHED,
         PreprocessingStrategy.DUMMY, 2);
   }
