ea5ddea0e073f153c59255b9d8f482eda1b62a7d
Peter Sebastian Nordholt <peter.s.nordholt@alexandra.dk>
2018-08-28 22:27:49 +0200

    Fix mistake in launchscript.

--- launchscript.sh
+++ launchscript.sh
@@ -7,9 +7,13 @@ if test "$#" -ne 2; then
 fi
 
 echo Running MNIST FL Demo with $1 parties each with $2 local examples
-for i in `seq 2 $1`;
-do
-java -jar target/fresco-ml-0.0.1-SNAPSHOT-jar-with-dependencies.jar -i $i -n $1 -l $2 &>/dev/null &
-done
+if test $1 -ne 1; then 
+    for i in `seq 2 $1`;
+    do
+        echo Starting party $i
+        java -jar target/fresco-ml-0.0.1-SNAPSHOT-jar-with-dependencies.jar -i $i -n $1 -l $2 &>/dev/null &
+    done
+fi
+echo Starting party 1
 java -jar target/fresco-ml-0.0.1-SNAPSHOT-jar-with-dependencies.jar -i 1 -n $1 -l $2
 echo Ran MNIST FL Demo with $1 parties each with $2 local examples
