c35166aea26220cff38128a783a748f1db95539c
Nikolaj Volgushev <nikolajvolgushev@Nikolajs-MacBook-Pro-2.local>
2019-01-15 20:38:56 +0100

    Bump version

--- src/test/java/dk/alexandra/fresco/ml/dtrees/TestDecisionTreeComputationsSpdz.java
+++ src/test/java/dk/alexandra/fresco/ml/dtrees/TestDecisionTreeComputationsSpdz.java
@@ -1,6 +1,5 @@
 package dk.alexandra.fresco.ml.dtrees;
 
-import dk.alexandra.fresco.framework.sce.evaluator.EvaluationStrategy;
 import dk.alexandra.fresco.suite.spdz.AbstractSpdzTest;
 import dk.alexandra.fresco.suite.spdz.configuration.PreprocessingStrategy;
 import org.junit.Test;
@@ -10,28 +9,24 @@ public class TestDecisionTreeComputationsSpdz extends AbstractSpdzTest {
   @Test
   public void testEvaluateDecisionTree() {
     runTest(new DecisionTreeComputationTests.TestEvaluateDecisionTree<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED,
         PreprocessingStrategy.DUMMY, 2);
   }
 
   @Test
   public void testEvaluateDecisionTreeFour() {
     runTest(new DecisionTreeComputationTests.TestEvaluateDecisionTreeFour<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED,
         PreprocessingStrategy.DUMMY, 2);
   }
 
   @Test
   public void testEvaluateDecisionTreeFive() {
     runTest(new DecisionTreeComputationTests.TestEvaluateDecisionTreeFive<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED,
         PreprocessingStrategy.DUMMY, 2);
   }
 
   @Test
   public void testEvaluateDecisionTreeSix() {
     runTest(new DecisionTreeComputationTests.TestEvaluateDecisionTreeSix<>(),
-        EvaluationStrategy.SEQUENTIAL_BATCHED,
         PreprocessingStrategy.DUMMY, 2);
   }
 
--- src/test/java/dk/alexandra/fresco/ml/svm/TestSVMComputationsSpdz.java
+++ src/test/java/dk/alexandra/fresco/ml/svm/TestSVMComputationsSpdz.java
@@ -1,6 +1,5 @@
 package dk.alexandra.fresco.ml.svm;
 
-import dk.alexandra.fresco.framework.sce.evaluator.EvaluationStrategy;
 import dk.alexandra.fresco.suite.spdz.AbstractSpdzTest;
 import dk.alexandra.fresco.suite.spdz.configuration.PreprocessingStrategy;
 import org.junit.Test;
@@ -9,7 +8,7 @@ public class TestSVMComputationsSpdz extends AbstractSpdzTest {
 
   @Test
   public void testEvaluateSVM() {
-    runTest(new SVMComputationTests.TestEvaluateSVM<>(), EvaluationStrategy.SEQUENTIAL_BATCHED,
-        PreprocessingStrategy.DUMMY, 2, false, 128, 64, 16);
+    runTest(new SVMComputationTests.TestEvaluateSVM<>(),
+        PreprocessingStrategy.DUMMY, 2, 128, 64, 16);
   }
 }
